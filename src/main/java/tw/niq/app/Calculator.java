dd0b4da4f5765e2294cfdd82adeb0759a0037fd3
Nick Chen <sakanasan20@gmail.com>
2022-06-28 00:40:48 +0800

    Add calculator app and test

---
+++ src/main/java/tw/niq/app/Calculator.java
@@ -0,0 +1,9 @@
+package tw.niq.app;
+
+public class Calculator {
+	
+	public int integerDivision(int dividend, int dicisor) {
+		return dividend / dicisor;
+	}
+
+}
---
+++ src/test/java/tw/niq/app/CalculatorTest.java
@@ -0,0 +1,12 @@
+package tw.niq.app;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void test() {
+		System.out.println("Not yet implemented");
+	}
+
+}
