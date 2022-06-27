6bb3351d25f928670248b2855807f20274ed7e05
Nick Chen <sakanasan20@gmail.com>
2022-06-28 00:36:37 +0800

    Initial commit

---
+++ src/main/java/tw/niq/app/SpringBootJunitExampleApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJunitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJunitExampleApplication.class, args);
+	}
+
+}
