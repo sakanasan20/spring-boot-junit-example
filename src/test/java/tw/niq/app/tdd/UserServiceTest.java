237c2f2be9adc7e7698445b4d5e0ee3ed0e06ae8
nick.chen <sakanasan20@gmail.com>
2022-06-28 13:06:49 +0800

    Create user service test

---
+++ src/test/java/tw/niq/app/tdd/UserServiceTest.java
@@ -0,0 +1,18 @@
+package tw.niq.app.tdd;
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+	@Test
+	void testCreateUser_whenUserDetailsProvided_returnUserObject() {
+		
+		// Arrange
+
+		// Act
+		
+		// Assert
+		
+	}
+	
+}
